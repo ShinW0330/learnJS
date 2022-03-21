40d7abf0f01706031f0a9eb9af22784622427053
matai-shin <shinwook0330@gmail.com>
2022-03-21 17:34:10 +0900

    `${ }` 사용

---
+++ functionuserNameCopy(username).js
@@ -0,0 +1,19 @@
+function userNameCopy (username){
+  console.log(`${username}입니다.`);
+}
+
+userNameCopy('shin');
+userNameCopy('gu');
+userNameCopy('kang');
+userNameCopy('park');
+userNameCopy('jin');
+
+function userNameCopy (name){
+  console.log(name + "입니다.");
+}
+
+userNameCopy('shin');
+userNameCopy('gu');
+userNameCopy('kang');
+userNameCopy('park');
+userNameCopy('jin');
