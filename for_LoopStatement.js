2f849bfaf3bd2c301b23b9b99e65b2771511393a
matai-shin <shinwook0330@gmail.com>
2022-03-24 16:38:13 +0900

    for 높이에 따른 * 찍어보기

--- for_LoopStatement.js
+++ for_LoopStatement.js
@@ -75,3 +75,24 @@ for(let i = 2; i<= 100 ; i += 2){
   console.log(i);
   
 }
+
+
+// 별 찍어 보기 
+function printTriangle(height) {
+	// 여기에 코드를 작성해 주세요.
+	let heightMsg ='';
+	for(i=0; i <height; i++){
+	  heightMsg = heightMsg +'*';
+	  console.log(heightMsg);
+	}
+}
+
+// 테스트 코드
+console.log('높이: 1');
+printTriangle(1);
+
+console.log('높이: 3');
+printTriangle(3);
+
+console.log('높이: 5');
+printTriangle(5);
