241f713f48ddef9364d440d07601dec7102ea957
matai-shin <shinwook0330@gmail.com>
2022-03-24 12:08:11 +0900

    switch와 if문

--- switch.js
+++ switch.js
@@ -64,3 +64,17 @@ switch(myChoice2){
   default:
     console.log('1에서 4사이의 숫자를 선택해 주세요.');
 }
+
+//if 문으로 작성
+
+if (myChoice === 1) {
+  console.log('토끼를 선택한 당신, ...');
+} else if (myChoice === 2) {
+  console.log('고양이를 선택한 당신, ...');
+} else if (myChoice === 3) {
+  console.log('코알라를 선택한 당신, ...');
+} else if (myChoice === 4) {
+  console.log('강아지를 선택한 당신, ...');
+} else {
+  console.log('1에서 4사이의 숫자를 선택해 주세요.');
+}
