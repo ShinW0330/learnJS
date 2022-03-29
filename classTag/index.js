abb704c93e18eccfcc94e14c12f716d347d8fab0
matai-shin <shinwook0330@gmail.com>
2022-03-29 17:32:08 +0900

    HTMLCollection 유사 배열에 대해서

--- classTag/index.js
+++ classTag/index.js
@@ -1,17 +1,29 @@
-const myTags1 = document.getElementsByClassName('color-btn');
-console.log(myTags1);
-console.log(myTags1[1]);
-console.log(myTags1.length);
+/**
+ * 유사배열 (Array-Like Object)
+ * 여러 개의 값을 담고 있는 배열과 형태는 유사하지만
+ * 배열의 메소드는 사용할 수 없는 것들을 가리켜서 유사 배열이라고 한다,. 
+ * 
+ */
 
-for (let tag of myTags1) {
-	console.log(tag);
+const myTag1 = document.getElementsByClassName('color-btn');
+console.log(myTag1); // HTMLCollection(8) 은 color-btn 태그의 속성들을 표시 
+// 배열의 메서드를 사용 할수 없지만 
+console.log(myTag1[1]); // [] 를 하여 인덱스로 접근 하거나 
+// run button.color-btn.orange
+console.log(myTag1.length); // length 프로퍼티를 활용
+// run 8
+
+// 컬러에 대한 리스트가 나옴 
+for (let tag of myTag1){
+  console.log(tag);
 }
-
-const myTags2 = document.getElementsByClassName('red');
-console.log(myTags2);
-console.log(myTags2[0]);
-
-const myTags3 = document.getElementsByClassName('white');
-console.log(myTags3);
-console.log(myTags3 === null);
-console.log(myTags3.length);
+/* run 
+button.color-btn.red
+button.color-btn.orange
+button.color-btn.yellow
+button.color-btn.green
+button.color-btn.blue
+button.color-btn.indigo
+button.color-btn.purple
+button.color-btn.black
+*/
