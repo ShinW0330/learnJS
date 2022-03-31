d84209ca084a4d13f8d782db5dd3b92e49f195df
matai-shin <shinwook0330@gmail.com>
2022-04-01 03:00:24 +0900

    태그를 사용하여 클릭 및 동작은 js html은 구조로 사용

--- eventAndbutton/index.js
+++ eventAndbutton/index.js
@@ -2,8 +2,12 @@
 const btn = document.querySelector('#myBtn');
 
 // 이벤트 핸들링(Event Handling)
+
+// btn 변수에 onclick 이라는 프로퍼트를 사용하여 함수 정의 
 btn.onclick = function() { // 이벤트 핸들러(Event Handler)
 	console.log('Hello Codeit!');
+  alert('클릭');
 };
 
 // 이벤트 핸들링
+// 태그를 선택해서 프로퍼티에 어떤 함수를 할당하는게 
