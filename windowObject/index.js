5d43a4d05656f20288cebabb6bf07ad20434a1fe
matai-shin <shinwook0330@gmail.com>
2022-04-01 03:42:18 +0900

    window 객체에 대해서

---
+++ windowObject/index.js
@@ -0,0 +1,8 @@
+// window: 전역객체 Global Object
+console.log(window); // 우리가 사용하는 웹 브라우저의 모든 정보를 볼수 있다. 
+console.log(window.innerWidth); // 화면에 제공하는 가로 사이즈
+console.log(window.innerHeight);
+
+window.console.log(window);
+window.console.log(window.innerWidth);
+window.console.log(window.innerHeight);
