6939d33da26c8a0e128d6c4cbdfdd2456b7686fc
matai-shin <shinwook0330@gmail.com>
2022-04-16 22:16:52 +0900

    간단한todolist

---
+++ toDoList/index.js
@@ -0,0 +1,15 @@
+const toDoList = document.querySelector('#to-do-list');
+
+console.log(toDoList);
+
+function addList(text){
+  const liTag = document.createElement('li');
+  liTag.innerText  = text;
+  toDoList.append(liTag);
+}
+
+addList('모모랑 산책하기');
+addList('모모랑 공부하기');
+addList('모모랑 밥먹기');
+addList('모모 괴롭히기');
+addList('모모 물기')
