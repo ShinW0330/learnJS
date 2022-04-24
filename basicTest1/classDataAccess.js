317cee0fd2866d61742e6aa5a717dce2c4591aac
matai-shin <shinwook0330@gmail.com>
2022-04-25 00:00:14 +0900

    ss

--- basicTest1/classDataAccess.js
+++ basicTest1/classDataAccess.js
@@ -16,7 +16,7 @@ console.log(sin.boneYear);
 // 그렇기 때문에 변수 명에 ' ' 를 같이 써 주어야 변수로 사용 할수 있지만 
 // 이렇게 만들어진 변수명은 점 표기법으로는 접근할 수 없다. 
 
-// console.log(sin.bone Year2); 사용불가
+// console.log(sin.bone Year2); 사용불가 //ss
 
 // 대괄호 표기법 objectName['propertyName']
 console.log(sin['bone Year2']);
