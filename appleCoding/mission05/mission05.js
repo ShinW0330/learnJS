39d6bcdecd9ea5a53949192575770df49f9843d7
KANGSHINWOOK <shinwook0330@gmail.com>
2023-02-21 22:39:14 +0900

    슬라이드 연습

--- appleCoding/mission05/mission05.js
+++ appleCoding/mission05/mission05.js
@@ -1,2 +1,36 @@
-const slideContainerEl = document.querySelectorAll('.silde-container');
-const slideFadeEls = slideContainerEl
+let sildeIndex = 1;
+    slideFn(sildeIndex);
+
+
+function plus(chkNum){
+    console.log(sildeIndex = chkNum);
+    slideFn(sildeIndex = chkNum-1);
+}
+
+function prevNextChk(chkNum){
+    console.log(sildeIndex);
+    console.log(sildeIndex += chkNum);
+    slideFn(sildeIndex += chkNum+1);
+}
+
+function slideFn(n){
+   // const slideContainerEl = document.querySelectorAll('.silde-container');
+    const slideFadeEls = document.querySelectorAll('.slide-fade');
+    const btnsContainerEls = document.querySelectorAll('.btns-container')[0];
+   // const btnItemsEls =btnsContainerEls.querySelectorAll('.btn-item');
+    let i;
+    for (let i = 0; i < slideFadeEls.length; i++) {
+        slideFadeEls[i].classList.remove('active');            
+    }
+    // if(chkNum > slideFadeEls.length){ 
+    //     sildeIndex =1;
+    // };
+    // if(chkNum <= slideFadeEls.length){ 
+    //     sildeIndex = slideFadeEls.length;
+    // };
+    slideFadeEls[n].classList.add('active');
+    
+    
+    
+}
+// document.querySelectorAll('.slide-fade')[2].classList.add('active');
